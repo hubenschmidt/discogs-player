5e83af6a333740f8cc70b18cd4a093be7c312565
hubenschmidt <whubenschmidt@gmail.com>
2019-10-07 22:13:50 -0400

    adding user controller and api routes

---
+++ routes/api/user.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const RecordController = require("../controllers/RecordController");
+
+//matches with /api/user
+router.get('/',)
+
+// router.get('/', RecordController.getAllRecords);
+// router.post('/', RecordController.addRecord);
+// router.get('/:id', RecordController.getARecord);
+// router.put('/:id', RecordController.updatedRecord);
+// router.delete('/:id', RecordController.deleteRecord);
+
+// export default router;
+module.exports = router;
