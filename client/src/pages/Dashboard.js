bdd57a0724f3b0549af419631a6720319fb1ce46
hubenschmidt <whubenschmidt@gmail.com>
2019-10-23 09:20:43 -0400

    dashboard.js config and API url

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import API from "../utils/API"
+import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { Redirect } from "react-router-dom";
 import { withStyles } from "@material-ui/styles";
@@ -8,40 +8,45 @@ import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Box";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Box from "@material-ui/core/Box";
-const _ = require("lodash");
+
+import io from "socket.io-client";
+import OAuth from "../components/OAuth"
+// import { API_URL } from "../config";
+// const socket = io(API_URL);
+const providers = ["discogs"];
 
 //styling
 const styles = theme => ({
-  root: {
-    minHeight: "100vh",
-    backgroundColor: "#F4F4F4",
-  },
-  paper: {
-    // marginTop: "100px",
-    paddingLeft: "10px",
-    paddingRight: "10px",
-    backgroundColor: "#ECECEC"
-  },
-  panel: {
-    paddingLeft: "10px",
-    paddingRight: "10px"
-  },
-  input: {
-    width: "100%"
-  },
-  reports: {
-    textAlign: "right",
-    backgroundColor: "#77DB7E",
-    padding: "10px"
-  },
-  reportLinks: {
-    marginRight: "10vw"
-  }
+  // root: {
+  //   minHeight: "100vh",
+  //   backgroundColor: "#F4F4F4",
+  // },
+  // paper: {
+  //   // marginTop: "100px",
+  //   paddingLeft: "10px",
+  //   paddingRight: "10px",
+  //   backgroundColor: "#ECECEC"
+  // },
+  // panel: {
+  //   paddingLeft: "10px",
+  //   paddingRight: "10px"
+  // },
+  // input: {
+  //   width: "100%"
+  // },
+  // reports: {
+  //   textAlign: "right",
+  //   backgroundColor: "#77DB7E",
+  //   padding: "10px"
+  // },
+  // reportLinks: {
+  //   marginRight: "10vw"
+  // }
 });
 
 class Dashboard extends Component {
   state = {
-    user: {},
+    user: {}
   };
 
   componentWillMount() {
@@ -61,15 +66,24 @@ class Dashboard extends Component {
       return <Redirect to={{ pathname: "/login" }} />;
     } else {
       return (
-        <Fragment>
-          <CssBaseline />
-          <Container>
-            <Grid container>
-                <h1>Welcome to this Dashboard</h1>
-
-            </Grid>
-          </Container>
-        </Fragment>
+        <div style={{ height: "75vh" }} className="container valign-wrapper">
+          {providers.map(provider => (
+            <OAuth provider={provider} key={provider} socket={socket} />
+          ))}
+          <div className="row">
+            <div className="landing-copy col s12 center-align">
+              <h4>
+                <p className="flow-text grey-text text-darken-1">
+                  You are logged into{" "}
+                  <span style={{ fontFamily: "monospace" }}>
+                    discogs-player
+                  </span>{" "}
+                  app. Would you like to authorize discogs?
+                </p>
+              </h4>
+            </div>
+          </div>
+        </div>
       );
     }
   }
