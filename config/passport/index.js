9cd3bfaf6f00c8b6a8c977c07008f6c4b8e1ac23
hubenschmidt <whubenschmidt@gmail.com>
2019-10-27 21:43:47 -0400

    user object saved in deserialize. app authorizes but request data being sent back from server is not persisted, just displayed in node server console

--- config/passport/index.js
+++ config/passport/index.js
@@ -7,34 +7,30 @@ const { DISCOGS_CONFIG } = require("../oauth/index.js");
 // called on login, saves the id to session req.session.passport.user = {id:'..'}
 passport.serializeUser((user, done) => {
   // console.log("*** serializeUser called, user: ");
-  // console.log(user); // the whole raw user object!
+  console.log(user); // the whole raw user object!
   // console.log("---------");
-  done(null, { id: user.id });
+  // done(null, { id: user.id });
+  done(null, user)
+
 });
 
 // user object attaches to the request as req.user
-passport.deserializeUser((id, done) => {
-  console.log("DeserializeUser called", id);
-  db.User.findAll({
-    where: { id: id.id },
-  }).then(function(user){
-    console.log('user id callback', user)
-  // db.User.findByPk(id.id).then(function(user) {
-  // .then(function(user) {
-    if (user) {
-      done(null, user.get());
-    } else {
-      done(user.errors, null);
-    }
-  });
-
-  // db.User.findByPk({ id }, "username", (err, user) => {
-  //   console.log("*** Deserialize user, user:");
-  //   console.log(user);
-  //   console.log("--------------");
-  //   done(null, user);
-  // });
-});
+// passport.deserializeUser((id, done) => {
+//   console.log("DeserializeUser called", id);
+//   db.User.findAll({
+//     where: { id: id.id },
+//   }).then(function(user){
+//     console.log('user id callback', user)
+//   // db.User.findByPk(id.id).then(function(user) {
+//   // .then(function(user) {
+//     if (user) {
+//       done(null, user.get());
+//     } else {
+//       done(user.errors, null);
+//     }
+//   });
+// });
+passport.deserializeUser((obj, cb) => cb(null, obj))
 
 //the callback that is invoked when an OAuth provider sends back user information. Normally, you would save the user to the database in this callback and it would be customized for each provider.
 
