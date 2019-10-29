31022361ac420bf284a917582bbe83a80bacda7e
hubenschmidt <whubenschmidt@gmail.com>
2019-10-28 21:10:38 -0400

    rewriting OAuth with disconnect npm module

---
+++ config/middleware/discogs.js
@@ -0,0 +1,8 @@
+// var Discogs = require('disconnect').Client;
+
+// var db = new Discogs().database();
+// db.getRelease(176126, function(err, data){
+// 	console.log(data);
+// });
+
+// module.exports = db;
--- routes/auth/discogs.js
+++ routes/auth/discogs.js
@@ -2,14 +2,33 @@ const express = require("express");
 const router = express.Router();
 const passport = require("../../config/passport");
 const authController = require("../../controllers/AuthController");
+const Discogs = require('disconnect').Client;
 
 // setting up the passport middleware for each of the OAuth providers
-const discogsAuth = passport.authenticate("discogs");
+// const discogsAuth = passport.authenticate("discogs");
+// const discogsAuth = require("../../config/middleware/discogs")
 
 // routes that are triggered by the callbacks from each OAuth provider once the user has authenticated successfully.
 
+// routes that are triggered on the client.
+// matches with /auth/discogs
+router.get('/discogs', function(req, res){
+	var oAuth = new Discogs().oauth();
+	oAuth.getRequestToken(
+		'ucyQbMxfuVNEigpgyQrp', 
+		'hJkdzVOPODpOErIWzhkKgUeBJDQlqAEt', 
+		'https://localhost:5000/auth/discogs/callback', 
+		function(err, requestData){
+      // Persist "requestData" here so that the callback handler can 
+      console.log(requestData)
+			// access it later after returning from the authorize url
+			res.redirect(requestData.authorizeUrl);
+		}
+	);
+});
+
 //matches with /auth/discogs/callback
-router.get("/discogs/callback", discogsAuth, authController.discogs);
+router.get("/discogs/callback", authController.discogs);
 
 //this custom middleware allows us to attack the socket id to the session.
 // With that socket id we can send back the right user info to the right socket.
@@ -18,8 +37,6 @@ router.use((req, res, next) => {
   next();
 });
 
-// routes that are triggered on the client.
-// matches with /auth/discogs
-router.get('/discogs', discogsAuth);
+
 
 module.exports = router;
