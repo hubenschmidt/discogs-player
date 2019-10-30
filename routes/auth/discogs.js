c93d7e987ff01156b3ce61c7bcc23e1af907e013
hubenschmidt <whubenschmidt@gmail.com>
2019-10-29 22:09:13 -0400

    saving auth back onto discogs/auth route

--- routes/auth/discogs.js
+++ routes/auth/discogs.js
@@ -1,53 +1,44 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../../controllers/AuthController")
+const authController = require("../../controllers/AuthController");
 const { DISCOGS_CONFIG } = require("../../config/oauth/index");
 
 const Discogs = require("disconnect").Client;
 
+// routes that are triggered on the client.
+// matches with /auth/discogs
 
 // routes that are triggered on the client.
 // matches with /auth/discogs
-router.get("/discogs", authController.requestToken);
+router.get("/discogs", function(req, res) {
+  var oAuth = new Discogs().oauth();
+  oAuth.getRequestToken(
+    DISCOGS_CONFIG.consumerKey,
+    DISCOGS_CONFIG.consumerSecret,
+    DISCOGS_CONFIG.callbackURL,
+    function(err, requestData) {
+      // Persist "requestData" here so that the callback handler can
+      // access it later after returning from the authorize url
+      console.log("request data here", requestData);
+      res.redirect(requestData.authorizeUrl);
 
-// matches with /auth/discogs
-router.get("/discogs/callback", authController.accessToken);
-
-
-
-
-
-// // routes that are triggered on the client.
-// // matches with /auth/discogs
-// router.get("/discogs", function(req, res) {
-//   var oAuth = new Discogs().oauth();
-//   oAuth.getRequestToken(
-//     DISCOGS_CONFIG.consumerKey,
-//     DISCOGS_CONFIG.consumerSecret,
-//     DISCOGS_CONFIG.callbackURL,
-//     function(err, requestData) {
-//       // Persist "requestData" here so that the callback handler can
-//       // access it later after returning from the authorize url
-//       console.log("request data here", requestData);
-//       res.redirect(requestData.authorizeUrl);
-
-//       //matches with /auth/discogs/callback
-//       // router.get("/discogs/callback", authController.discogs);
-//       router.get("/discogs/callback", function(req, res) {
-//         var oAuth = new Discogs(requestData).oauth();
-//         oAuth.getAccessToken(
-//           req.query.oauth_verifier, // Verification code sent back by Discogs
-//           function(err, accessData) {
-//             // Persist "accessData" here for following OAuth calls
-//             console.log("accessData here", accessData);
-//             // res.send("Received access token!");
-//             res.send("<script>window.close()</script>");
-//           }
-//         );
-//       });
-//     }
-//   );
-// });
+      //matches with /auth/discogs/callback
+      // router.get("/discogs/callback", authController.discogs);
+      router.get("/discogs/callback", function(req, res) {
+        var oAuth = new Discogs(requestData).oauth();
+        oAuth.getAccessToken(
+          req.query.oauth_verifier, // Verification code sent back by Discogs
+          function(err, accessData) {
+            // Persist "accessData" here for following OAuth calls
+            console.log("accessData here", accessData);
+            // res.send("Received access token!");
+            res.send("<script>window.close()</script>");
+          }
+        );
+      });
+    }
+  );
+});
 
 // matches with auth/discogs/identity
 // testing discogs authentication using token and secret
