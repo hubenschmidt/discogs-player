b005911dd032ba29f393e053e9017fcfa4f9995d
hubenschmidt <whubenschmidt@gmail.com>
2019-11-01 00:02:20 -0400

    saving discogsController

--- controllers/DiscogsController.js
+++ controllers/DiscogsController.js
@@ -1,11 +1,12 @@
 //test controller methods in postman
 //after tests succeed, break into service/controller/model architecture
 const db = require("../models");
+const Discogs = require("disconnect").Client;
 
 module.exports = {
   test: test,
-//   getUserData: getUserData,
-//   getUserCollection: getUserCollection,
+  //   getUserData: getUserData,
+  //   getUserCollection: getUserCollection,
   sync: sync
 };
 
@@ -15,38 +16,44 @@ async function test(req, res) {
   res.send("welcome to discogs api route");
 }
 
-async function getUserData (id) {
-    return new Promise((resolve, reject) => {
-        resolve('testing getUserData promise')
-    //   db.User.findByPk(id, function(err, doc) {
-    //     if (err) reject(err);
-    //     resolve(doc ? doc.toJSON() : undefined);
+async function getUserData(id) {
+  return new Promise((resolve, reject) => {
+    try {
+        resolve(db.User.findByPk(id))
+    //   db.User.findByPk(id, function(err, dbModel) {
+    //     // if (err) reject(err);
+    //     resolve(dbModel)
+    //     // resolve(dbModel ? dbModel.toJSON() : undefined);
     //   });
-    });
-  };
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
 
 async function getUserCollection(userId) {
   var userData = await getUserData(userId);
-  console.log(userData)
- 
+//   console.log(userData, 'USER DATA HERE')
+
+  //   const dis = new Discogs(accessData).database();
+
   // var accessData = userData
   //construct accessData from userData promise response====================-
 
-//   var col = new Discogs(accessData).user().collection();
+  //   var col = new Discogs(accessData).user().collection();
   return new Promise((resolve, reject) => {
-      
     try {
-        resolve('testing getUserCollection promise')
-    //   col.getReleases(
-    //     userData.discogsUserData.username,
-    //     0,
-    //     //configure to return entire paginated collection
-    //     { page: 1, per_page: 256 },
-    //     function(err, data) {
-    //       if (err) reject(err);
-    //       resolve(data ? data.releases : null);
-    //     }
-    //   );
+      resolve("testing getUserCollection promise");
+      //   col.getReleases(
+      //     userData.discogsUserData.username,
+      //     0,
+      //     //configure to return entire paginated collection
+      //     { page: 1, per_page: 256 },
+      //     function(err, data) {
+      //       if (err) reject(err);
+      //       resolve(data ? data.releases : null);
+      //     }
+      //   );
     } catch (e) {
       reject(e);
     }
@@ -55,35 +62,34 @@ async function getUserCollection(userId) {
 
 async function sync(req, res) {
   var userId = req.params._id;
-  res.json(userId)
   var releases = await getUserCollection(userId);
-  console.log(releases, 'logging releases')
-//   await asyncForEach(releases, async release => {
-//     var releaseId = release.id;
-//     var existing = await dbFindOneByReleaseId(releaseId);
-//     if (existing) {
-//       existing = existing.toJSON();
-//       var update = false;
-//       if (existing.userIds) {
-//         existing.userIds = existing.userIds.map(uid => uid.toString());
-//         if (existing.userIds.indexOf(userId) == -1) {
-//           existing.userIds.push(userId);
-//           update = true;
-//         }
-//       } else {
-//         existing.userIds = [userId];
-//         update = true;
-//       }
-//       if (update)
-//         await findOneAndUpdatePromise(
-//           { _id: existing._id },
-//           { userIds: existing.userIds }
-//         );
-//     } else {
-//       const dbRel = formatResponse(release);
-//       dbRel[0].userIds = [userId];
-//       await createReleasePromise(dbRel);
-//     }
-//   });
-//   res.json(releases);
+  //   console.log('logging releases', releases)
+  //   await asyncForEach(releases, async release => {
+  //     var releaseId = release.id;
+  //     var existing = await dbFindOneByReleaseId(releaseId);
+  //     if (existing) {
+  //       existing = existing.toJSON();
+  //       var update = false;
+  //       if (existing.userIds) {
+  //         existing.userIds = existing.userIds.map(uid => uid.toString());
+  //         if (existing.userIds.indexOf(userId) == -1) {
+  //           existing.userIds.push(userId);
+  //           update = true;
+  //         }
+  //       } else {
+  //         existing.userIds = [userId];
+  //         update = true;
+  //       }
+  //       if (update)
+  //         await findOneAndUpdatePromise(
+  //           { _id: existing._id },
+  //           { userIds: existing.userIds }
+  //         );
+  //     } else {
+  //       const dbRel = formatResponse(release);
+  //       dbRel[0].userIds = [userId];
+  //       await createReleasePromise(dbRel);
+  //     }
+  //   });
+  //   res.json(releases);
 }
