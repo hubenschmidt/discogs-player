812b19bc9f48df38e2d80cba16a933ed7db3e917
hubenschmidt <whubenschmidt@gmail.com>
2020-03-07 13:19:11 -0500

    adding

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
-const config = require("dotenv");
-const express = require("express");
-const bodyParser = require("body-parser")
-const path = require("path");
-const db = require("./models");
-const morgan = require("morgan");
-const session = require("express-session");
-const { CLIENT_ORIGIN } = require("./config/oauth")
-const cors = require("cors");
+const config = require('dotenv');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const db = require('./models');
+const morgan = require('morgan');
+const session = require('express-session');
+const { CLIENT_ORIGIN } = require('./config/oauth');
+const cors = require('cors');
 
-const fs = require('fs')
+const fs = require('fs');
 const https = require('https');
 const http = require('http');
 const socketio = require('socket.io');
 let server;
 
 // const passportInit = require('./lib/passport.init');
-
 // initalize sequelize with session store
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const cookieParser = require('cookie-parser')
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const cookieParser = require('cookie-parser');
 
 //requiring passport as configured
-const passport = require("./config/passport")
+const passport = require('./config/passport');
 
 //dotenv config
 config.config();
@@ -45,17 +44,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(cors({origin: CLIENT_ORIGIN}));
 const corsOptions = {
   origin: 'http://localhost:3000',
-  credentials: true,
-}
+  credentials: true
+};
 
-app.use(cors(corsOptions))
-
+app.use(cors(corsOptions));
 
 // If we are in production we are already running in https
 if (process.env.NODE_ENV === 'production') {
-  server = http.createServer(app)
+  server = http.createServer(app);
 }
-// We are not in production so load up our certificates to be able to 
+// We are not in production so load up our certificates to be able to
 // run the server in https mode locally
 else {
   const certOptions = {
@@ -63,28 +61,30 @@ else {
     // key: fs.readFileSync(path.resolve('certs/server.key')),
     cert: fs.readFileSync(path.resolve('certs/certificate.pem'))
     // cert: fs.readFileSync(path.resolve('certs/server.crt'))
-  }
+  };
   // console.log('certOptions', certOptions)
-  server = https.createServer(certOptions, app)
+  server = https.createServer(certOptions, app);
 }
 
 //sessions
 const myStore = new SequelizeStore({
   db: db.sequelize,
-  table: "Session",
+  table: 'Session',
   extendDefaultFields: extendDefaultFields,
   checkExpirationInterval: 15 * 60 * 1000, //the interval at which to cleanup expired sessions in milliseconds.
   expiration: 24 * 60 * 60 * 1000 //the maximum wage (in milliseconds) of a valid session.
-})
-app.use(session({
-  secret: process.env.COOKIE_SECRET,
-  store: myStore,
-  resave: false,
-  proxy: true,
-  saveUninitialized: false,
-}))
+});
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    store: myStore,
+    resave: false,
+    proxy: true,
+    saveUninitialized: false
+  })
+);
 
-function extendDefaultFields(defaults, session){
+function extendDefaultFields(defaults, session) {
   return {
     data: defaults.data,
     expires: defaults.expires,
@@ -93,15 +93,15 @@ function extendDefaultFields(defaults, session){
 }
 
 //passport
-app.use(passport.initialize())
-app.use(passport.session()) // calls the deserializer
+app.use(passport.initialize());
+app.use(passport.session()); // calls the deserializer
 
 // //set up routes
 app.use(routes);
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
 // Send every request to the React app
@@ -117,23 +117,21 @@ if (process.env.NODE_ENV === "production") {
 
 // app.get('*', (req, res) => res.sendFile(path.resolve('./client/build', 'index.html')));
 
-// Connecting sockets to the server and adding them to the request 
+// Connecting sockets to the server and adding them to the request
 // so that we can access them later in the controller
-const io = socketio(server)
-app.set('io', io)
+const io = socketio(server);
+app.set('io', io);
 
-
-// Catch a start up request so that a sleepy Heroku instance can  
+// Catch a start up request so that a sleepy Heroku instance can
 // be responsive as soon as possible
-app.get('/wake-up', (req, res) => res.send('👍'))
+app.get('/wake-up', (req, res) => res.send('👍'));
 
-
-db.sequelize.sync().then(function(){
+db.sequelize.sync().then(function() {
   // app.listen(PORT, function() {
   server.listen(PORT, function() {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
   });
-})
+});
 
 // export default app;
 
