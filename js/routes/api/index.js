ac90b54c7cba0ad5d9301d16c631f821509ed819
hubenschmidt <whubenschmidt@gmail.com>
2020-03-08 14:17:36 -0400

    adding index.ts . next need to create declaration file for thrd-party module connect-session-sequelize

--- js/routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const discogsRoutes = require("./discogs");
-const userRoutes = require("./user");
-
-// matches with api/discogs
-router.use("/user", userRoutes);
-router.use("/discogs", discogsRoutes);
-
-module.exports = router;
--- js/routes/auth/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const discogs = require("./discogs")
-
-// matches with /auth
-router.use("/", discogs);
-
-module.exports = router;
